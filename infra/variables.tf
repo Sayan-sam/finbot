c4431b32be5c48f04128a586fa52b9c92215f946
Sayan-sam <sayanb1729@gmail.com>
2025-05-31 21:08:30 +0000

    Removed Profile section

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,11 +3,6 @@ variable "region" {
     default = "ap-south-1"
 }
 
-variable "profile" {
-    description = "AWS profile"
-    default = "default"
-}
-
 variable "ec2_public_key" {
     description = "SSH public key"
     default = "assets/finbot_id_rsa.pub"
